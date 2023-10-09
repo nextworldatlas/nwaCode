ebb51516d64812498608942e8b9a6c32ce0f5b99
nextworldatlas <nextworldatlas@gmail.com>
2023-10-09 17:02:24 -0400

    Change description, load logo for sidebar, update sidebar links

--- src/SiteNavBar.js
+++ src/SiteNavBar.js
@@ -7,9 +7,10 @@ import LocalGroceryStoreTwoToneIcon from '@mui/icons-material/LocalGroceryStoreT
 import HomeTwoToneIcon from '@mui/icons-material/HomeTwoTone'
 import SettingsTwoToneIcon from '@mui/icons-material/SettingsTwoTone'
 import { FormControlLabel, Switch } from '@mui/material'
+import "./LogoNoScript.png"
 
 // right-menu element
-const rightMenuStyle = { 
+const rightMenuStyle = {
   zIndex: 10000,
   width: '250px',
   background: 'radial-gradient(circle at 3% -80%,#ADD8E6 0,#e3eedb 100%,violet 0)'
@@ -23,39 +24,39 @@ const rightMenuStyle = {
  * @param {Boolean} setShowLabels Assignment function for showLabels
  * @returns 
  */
-const SiteNavBar = ({showTutorial, setShowTutorial, showLabels, setShowLabels}) => {
-  return(
+const SiteNavBar = ({ showTutorial, setShowTutorial, showLabels, setShowLabels }) => {
+  return (
     <div className="navbar">
-      <Button size="small" color="primary" aria-label="link to home" style={{padding: '1px'}} href="https://www.nextworldatlas.com">
+      <Button size="small" color="primary" aria-label="link to home" style={{ padding: '1px' }} href="https://www.instagram.com/nextworldatlas" target="_blank">
         <div className="navbar-text">
-          <HomeTwoToneIcon fontSize="small"/>
-          <div>Home</div>
+          <HomeTwoToneIcon fontSize="small" />
+          <div>All Videos</div>
         </div>
       </Button>
-      <Button size="small" color="primary" aria-label="link to shop" style={{padding: '1px'}} href="https://www.etsy.com/shop/nextworldatlas">
+      <Button size="small" color="primary" aria-label="link to shop" style={{ padding: '1px' }} href="https://www.etsy.com/shop/nextworldatlas" target="_blank">
         <div className="navbar-text">
-          <LocalGroceryStoreTwoToneIcon fontSize="small"/>
+          <LocalGroceryStoreTwoToneIcon fontSize="small" />
           <div>Shop</div>
         </div>
       </Button>
       <div className="add-node-vertical" >
-        <Button size="small" color="primary" aria-label="controls menu" style={{padding: '1px'}} >
+        <Button size="small" color="primary" aria-label="controls menu" style={{ padding: '1px' }} >
           <div className="navbar-text">
-            <SettingsTwoToneIcon fontSize="small"/>
+            <SettingsTwoToneIcon fontSize="small" />
             <div>Controls</div>
           </div>
         </Button>
         <ul className="right-menu" style={rightMenuStyle}>
           <li>
-            <FormControlLabel control={<Switch checked={showTutorial} onChange={()=>setShowTutorial(prev=>!prev)}/>} label="Tutorial" />
+            <FormControlLabel control={<Switch checked={showTutorial} onChange={() => setShowTutorial(prev => !prev)} />} label="Tutorial" />
           </li>
           <li>
-            <FormControlLabel control={<Switch checked={showLabels}   onChange={()=>setShowLabels(prev=>!prev)} />} label="Modern Country Labels" />
+            <FormControlLabel control={<Switch checked={showLabels} onChange={() => setShowLabels(prev => !prev)} />} label="Modern Country Labels" />
           </li>
         </ul>
       </div>
     </div>
   )
 }
- 
+
 export default SiteNavBar
